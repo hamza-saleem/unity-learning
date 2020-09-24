3d79468880767c8144aaf077a26dc8daf18dc2f2
Muhammad Hamza Saleem <70068443@student.uol.edu.pk>
2020-09-25 01:57:19 +0500

    A simple FPS game

---
+++ Practice001/Assets/Scripts/GunFire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    void Update () {
+  if(Input.GetButtonDown("Fire1")) {
+   AudioSource gunsound  = GetComponent<AudioSource>();
+   gunsound.Play();
+   GetComponent<Animation>().Play("GunShot");
+  }
+ }
+}
